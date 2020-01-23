96f36f43e92af1f38ad1ef8a1b9d47a2723304ed
Aruta1ru <djgraf95@gmail.com>
2020-01-24 01:34:27 +0300

    Added PUT request on click "Выполнено"

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,27 +1,52 @@
-var bypassApi = Vue.resource('/bypass/{/id}');
+function getIndex(list, id) {
+    for (var i=0; i < list.length; i++) {
+        if (list[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+var bypassApi = Vue.resource('/bypass{/id}');
 
 Vue.component('bypass-row', {
-    props: ['bypass'],
+    props: ['bypass', 'bypasses'],
     template: '<tr>'+
-                    '<td>{{ bypass.id }}</td>'+
-                    '<td>{{ bypass.executor }}</td>'+
-                    '<td>{{ bypass.address }}</td>'+
-                    '<td>{{ bypass.dog_type }}</td>'+
-                    '<td>{{ bypass.bypass_date }}</td>'+
-                    '<td>{{ bypass.done_type }}</td>'+
-               '</tr>'
+                    '<td style="padding: 5px 3px; text-align: center;  border: 2px solid black;">{{ bypass.id }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: left;  border: 2px solid black;">{{ bypass.executor }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: left;  border: 2px solid black;">{{ bypass.address }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: center;  border: 2px solid black;">{{ bypass.dog_type }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: center;  border: 2px solid black;">{{ bypass.bypass_date }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: center;  border: 2px solid black;">{{ bypass.done_type }}</td>'+
+                    '<td style="padding: 5px 3px; text-align: left;"><input type="button" value="Выполнено" @click="edit" /></td>'+
+               '</tr>',
+     methods: {
+           edit: function() {
+                bypassApi.update({id: this.bypass.id}, {done_type: "выполнено"}).then(result =>
+                    result.json().then(data => {
+                        var index = getIndex(this.bypasses, data.id);
+                        this.bypasses.splice(index, 1, data);
+                    })
+                );
+           }
+     }
 });
 
 Vue.component('bypasses-list', {
   props: ['bypasses'],
-  template: '<table>'+
-                    '<th>ID</th>'+
-                    '<th>Исполнитель</th>'+
-                    '<th>Адрес</th>'+
-                    '<th>Тип договора</th>'+
-                    '<th>Дата обхода</th>'+
-                    '<th>Выполнение</th>'+
-                    '<bypass-row v-for="bypass in bypasses" :key="bypass.id" :bypass="bypass" />'+
+  data: function() {
+        return {
+                bypass: null
+        }
+  },
+  template: '<table style="width: 80%;">'+
+                    '<th style="border: 2px solid red;">ID</th>'+
+                    '<th style="border: 2px solid red;">Исполнитель</th>'+
+                    '<th style="border: 2px solid red;">Адрес</th>'+
+                    '<th style="border: 2px solid red;">Тип договора</th>'+
+                    '<th style="border: 2px solid red;">Дата обхода</th>'+
+                    '<th style="border: 2px solid red;">Выполнение</th>'+
+                    '<bypass-row v-for="bypass in bypasses" :key="bypass.id" :bypass="bypass" :bypasses="bypasses" />'+
             '</table>',
   created: function() {
         bypassApi.get().then(result =>
